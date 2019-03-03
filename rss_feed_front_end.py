d03da211bc8849a8e8609bfeca7f8388cfb810e3
Francisco Vargas <fvargasp@akamai.com>
2019-03-03 15:47:42 -0600

    Auto-populated basic cards

--- rss_feed_front_end.py
+++ rss_feed_front_end.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, json, jsonify
 
 # Initialize flask object
 app = Flask(__name__)
@@ -8,9 +9,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/v1/get-all-articles', methods=['GET'])
-def get_all_articles():
-    return 'Hello World.', 201
+@app.route('/v1/get-articles', methods=['GET'])
+def get_articles():
+	# number_articles = request.args.get('number').lower()
+	# start_key = request.args.get('start')
+	# print(number_articles)
+	# print(start_key)
+
+	# Read dummy static JSON for response temporarily
+	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+	json_url = os.path.join(SITE_ROOT, "static/", "sample.json")
+	data = json.load(open(json_url))
+	print(data)
+	return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
